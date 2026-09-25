73f9be92ac8555ddd4d00b320e2fbbc09ba50d22
nhabibifardigikala <n.habibifar.digikala@gmail.com>
2026-09-25 14:25:36 +0330

    Add files via upload

--- ui/app.js
+++ ui/app.js
@@ -26,7 +26,7 @@ let flexPaused = false;
 let flexCancelRequested = false;
 let flexResumeResolver = null;
 const ENGINE_VERSION = '13.0.0';
-const EXPECTED_REMOTE_CONFIG_VERSION = 382;
+const EXPECTED_REMOTE_CONFIG_VERSION = 383;
 
 
 async function getActivityLogs(){ const x=await chrome.storage.local.get(['opsActivityLog']); return Array.isArray(x.opsActivityLog)?x.opsActivityLog:[]; }
